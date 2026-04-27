02ace64b130170c5219e6e57087c1e5e8c38b0c6
TonyHuang <hzx0109@gmail.com>
2026-04-27 16:54:30 -0700

    init

---
+++ inc/footer.php
@@ -0,0 +1,18 @@
+    <section class="footer">
+        <div class="mt-auto">
+            <div class="container">
+                <div class="copyright">
+                    <div>Copyright © <strong></strong> All Rights Reserved</div>
+                    <div class="terms">
+                        <a href="terms.php">Terms of Use</a>
+                        <a href="privacy.php">Privacy Policy</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
