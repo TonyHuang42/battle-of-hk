c3efb4fcca4c9cbb1680c8b9a16d124239d1426c
TonyHuang <hzx0109@gmail.com>
2026-05-01 14:41:33 -0700

    Update hero section image positioning and add icon to accordion button

--- guerrilla-resistance.php
+++ guerrilla-resistance.php
@@ -7,7 +7,7 @@ include 'inc/header.php';
 
 <main>
     <section class="hero">
-        <img src="img/guerrilla-resistance/banner.png" alt="游擊與抵抗" class="hero-img" style="object-position: 50% center;">
+        <img src="img/guerrilla-resistance/banner.png" alt="游擊與抵抗" class="hero-img" style="object-position: 44% center;">
         <img src="img/home/section_header-游擊與抵抗.svg" alt="游擊與抵抗" class="hero-title battle-of-hong-kong-hero-title">
     </section>
 
